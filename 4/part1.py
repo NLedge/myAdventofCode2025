2d93d3f4b5d0fb9abd7204a37cb1039819a05d9e
ledge <n.ledge@proton.me>
2025-12-11 15:02:43 -0800

    Working day 4 part1.py

--- 4/part1.py
+++ 4/part1.py
@@ -1,3 +1,19 @@
-with open("input",encoding ='utf-8') as f:
-    for line in f:
-
+# input must have more than 3 lines
+count = 0
+top = ''
+with open("input.txt",encoding ='utf-8') as f:
+    mid = f.readline()
+    low = f.readline()
+    while(mid):
+        for i in range(len(mid)):
+            n = 0
+            for a in [top,mid,low]:
+                for j in range(-1,2):
+                    if a and i+j >= 0 and i+j < len(a) and a[i+j] == '@':
+                        n += 1
+            if n < 5 and mid[i] == '@':
+                count += 1
+        top = mid
+        mid = low
+        low = f.readline()
+print(count)
